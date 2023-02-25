c85ef20118af873f595885642d20cdda8878a7ff
hieudoanm <hieumdoan@gmail.com>
2023-02-25 12:52:13 +0000

    Deploying to gh-pages from @ houseofmystery/nextjs-template@455d87adf732800325e404c06de292d3786f3ca7 🚀

---
+++ _next/static/tvJQdK0PrRDUg0tRobsRE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9e1d69080799a76c.js"],"/404":["static/chunks/pages/404-3f72e4336f29ee24.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
