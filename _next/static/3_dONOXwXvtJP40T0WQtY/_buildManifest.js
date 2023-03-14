7a9cb56a6d401bc6179507faf57d44590be74fc2
hieudoanm <hieumdoan@gmail.com>
2023-03-14 01:30:55 +0000

    Deploying to gh-pages from @ houseoftypescript/nextjs-template@c939fbd83ac5e8e65cb6dd0dd3578e926d52ff2e 🚀

--- _next/static/3_dONOXwXvtJP40T0WQtY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9e1d69080799a76c.js"],"/404":["static/chunks/pages/404-3f72e4336f29ee24.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
